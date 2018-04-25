bc3c259a15082c5672c1325e3428b24dd7fb6e93
applewhi <38227492+applewhi@users.noreply.github.com>
2018-04-24 18:21:59 -0700

    initial commit

--- minesweeper.js
+++ minesweeper.js
@@ -1,117 +1,161 @@
-class Game{
-  constructor(numberOfRows, numberOfColumns, numberOfBombs){
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
+let
+playerOneMoveOneType,
+playerOneMoveOneValue,
 
-  playMove(rowIndex, columnIndex){
-    this._board.flipTile(rowIndex, columnIndex);
+playerOneMoveTwoType,
+playerOneMoveTwoValue,
 
-    if(this._board.playerBoard[rowIndex][columnIndex] === 'B'){
-      console.log('Game over');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()){
-        console.log('You won!');
-    } else {console.log('Current Board');
-      this._board.print();}
-  }
-}
+playerOneMoveThreeType,
+playerOneMoveThreeValue,
 
+playerTwoMoveOneType,
+playerTwoMoveOneValue,
 
-class Board{
-  constructor(numberOfRows, numberOfColumns, numberOfBombs){
+playerTwoMoveTwoType,
+playerTwoMoveTwoValue,
 
-    this._numberOfBombs = numberOfBombs;
-    this._numberOfTiles = numberOfColumns * numberOfRows;
-    this._playerBoard = Board.generatePlayerBoard(numberOfRows,numberOfColumns);
-    this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns,numberOfBombs);
-  }
+playerTwoMoveThreeType,
+playerTwoMoveThreeValue;
 
-  get playerBoard(){
-    return this._playerBoard;
-  }
+function setPlayerMoves(Player,moveOneType,
+moveOneValue,moveTwoType,moveTwoValue,moveThreeType,moveThreeValue) {
 
-  flipTile(rowIndex, columnIndex){
-   if(this._playerBoard[rowIndex][columnIndex] !== ' '){
-    console.log('This tile has already been flipped!');
+  if (!moveOneType||!moveOneValue || !moveTwoType ||!moveTwoValue ||
+    !moveThreeType || !moveThreeValue){
+      return null;
+    }
+
+  if (!isValidMoveType(moveOneType)||!isValidMoveType(moveTwoType)
+    ||!isValidMoveType(moveThreeType)){
     return;}
-   else if (this._bombBoard[rowIndex][columnIndex] ==='B'){
-     this._playerBoard[rowIndex][columnIndex] = 'B';}
-   else {this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);}
 
-   this._numberOfTiles--;
+  if (!isValidMoveValue(moveOneValue)||!isValidMoveValue(moveTwoValue)
+    ||!isValidMoveValue(moveThreeValue)){
+    return;}
+
+  if ((moveOneValue + moveTwoValue + moveThreeValue) > 99){
+    return;
+    }
+
+  if (Player === 'Player One'){
+
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
   }
+  else if (Player === 'Player Two'){
 
-  getNumberOfNeighborBombs(rowIndex, columnIndex){
-    this._neighborOffsets =[
-      [-1,-1],
-      [-1,0],
-      [-1,1],
-      [0,-1],
-      [0,1],
-      [1,-1],
-      [1,0],
-      [1,1]
-    ];
-
-    this._numberOfRows = this._bombBoard.length;
-    this._numberOfColumns = this._bombBoard[0].length;
-
-    this._neighborOffsets.forEach(offset => {
-      this._neighborRowIndex = rowIndex + offset[0];
-      this._neighborColumnIndex = columnIndex + offset[1];
-
-      if (
-         this._neighborRowIndex >0 &&
-         this._neighborRowIndex <= this._numberOfRows &&
-         this._neighborColumnIndex > 0 &&
-         this._neighborColumnIndex <= this._numberOfColumns)
-        {if(this._bombBoard[this._neighborRowIndex][this._neighborColumnIndex] === 'B')
-          {this._numberOfBombs++;}
-        }
-      }); return this._numberOfBombs;
-  }
-
-
-  hasSafeTiles(){
-    return this._numberOfTiles !== this._numberOfBomb;
-  }
-
-  print(){
-    console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
-  };
-
-  static generatePlayerBoard(numberOfRows, numberOfColumns){
-    const board = [];
-    for(let i=0;i<=numberOfRows;i++){
-      const row = [];
-      for(let j=0;j<=numberOfColumns;j++){
-        row.push(' ');
-      } board.push(row);
-    } return board;
-  }
-
-  static generateBombBoard(numberOfRows,numberOfColumns,numberOfBombs){
-  const board = [];
-  for(let i=0;i<=numberOfRows;i++){
-    const row = [];
-    for(let j=0;j<=numberOfColumns;j++){
-      row.push(null);
-    } board.push(row);
-  }
-    let numberOfBombsPlaced = 0;
-
-    while (numberOfBombsPlaced < numberOfBombs){
-      //currently bombs may be placed on top of each other.
-      let randomRowIndex = Math.floor(Math.random()*numberOfRows);
-      let randomColumnIndex = Math.floor(Math.random()*numberOfColumns);
-
-      if (board[randomRowIndex][randomColumnIndex] !='B'){
-        board[randomRowIndex][randomColumnIndex]='B';
-        numberOfBombsPlaced++;
-        }
-      }return board;
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveThreeValue = moveThreeValue;
   }
 }
 
-const g = new Game(3,3,3);
-g.playMove(0,0);
+function isValidMoveType (moveType) {
+  return (moveType === 'rock')||(moveType === 'paper')||
+  (moveType === 'scissors');
+  }
+
+function isValidMoveValue(moveValue){
+    return (moveValue >= 1) && (moveValue <= 99);
+}
+
+function getMoveWinner(
+  playerOneMoveType,
+  playerOneMoveValue,
+  playerTwoMoveType,
+  playerTwoMoveValue) {
+      if (!playerOneMoveType ||
+          !playerOneMoveValue ||
+          !playerTwoMoveType ||
+          !playerTwoMoveValue) {
+          return null;}
+
+      if (playerOneMoveType === playerTwoMoveType){
+      //players pick same move and value
+        if (playerOneMoveValue === playerTwoMoveValue){
+            return 'Tie';
+     //players pick same move, but player 1 picks higher value
+        }   else if(playerOneMoveValue > playerTwoMoveValue){
+            return 'Player One';
+        }   else {
+            return 'Player Two';
+          }
+        }
+
+    //player 1 pics ROCK
+    if (playerOneMoveType === 'rock'){
+        if (playerTwoMoveType === 'scissors'){
+          return 'Player One';
+     //player2 picks PAPER
+        } else {
+            return 'Player Two';}
+                                           }
+     //player 1 picks PAPER
+     else if (playerOneMoveType === 'paper'){
+        if (playerTwoMoveType === 'scissors'){
+          return 'Player Two';}
+    //player2 picks ROCK
+          else {return 'Player One';}
+        }
+  else {//player 1 picks scissors
+    if (playerTwoMoveType === 'rock'){
+     return 'Player Two';}
+    //player2 picks paper
+    else {return 'Player One';}
+    }
+  }
+
+
+  function getRoundWinner(roundNumber) {
+
+        switch(roundNumber){
+        case 1:
+          return getMoveWinner(playerOneMoveOneType,playerOneMoveOneValue,
+          playerTwoMoveOneType,playerTwoMoveOneValue);
+          break;
+
+        case 2:
+          return getMoveWinner(playerOneMoveTwoType,playerOneMoveTwoValue,
+          playerTwoMoveTwoType,playerTwoMoveTwoValue);
+          break;
+
+        case 3:
+          return getMoveWinner(playerOneMoveThreeType,playerOneMoveThreeValue,
+          playerTwoMoveThreeType,playerTwoMoveThreeValue);
+        break;
+
+        default:
+            return null;
+    }
+  }
+
+  function getGameWinner(){
+    let playerOneWinCount=0;
+    let playerTwoWinCount=0;
+
+    for (roundNumber = 1; roundNumber <=3; roundNumber++){
+      let winner = getRoundWinner(roundNumber);
+
+      if (winner ==='Player One'){
+      playerOneWinCount+=1;
+    }
+      else if (winner ==='Player Two'){
+      playerTwoWinCount+=1;
+    }
+    /* it's a tie*/
+    }
+
+    if (playerOneWinCount > playerTwoWinCount){
+      return 'Player One';}
+    else if (playerOneWinCount < playerTwoWinCount){
+      return 'Player Two';}
+    else return 'Tie';
+
+    }
